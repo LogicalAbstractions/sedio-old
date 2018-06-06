c5157ab145a4bd9da6935d41ce62208b5ddf89a8
Bernhard Glück <business@bernhardglueck.com>
2018-06-07 01:44:51 +0200

    Updated CI script

--- build.cmd
+++ build.cmd
@@ -1,3 +1,13 @@
+IF "%APPVEYOR_BUILD_VERSION%" == "" GOTO NOPATH
+:YESPATH
+@ECHO APPVEYOR_BUILD_VERSION environment variable was detected.
+GOTO END
+:NOPATH
+@ECHO The APPVEYOR_BUILD_VERSION environment variable was NOT detected.
+SET APPVEYOR_BUILD_VERSION=1.0.0
+GOTO END
+:END
+
 cd src\server\Sedio.Server
 dotnet restore
 dotnet build -c Release
@@ -5,9 +15,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\server\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\server\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\server\win-x64
 
-7z a -tzip ..\..\..\publish\sedio-server-linux-x64 ..\..\..\publish\server\linux-x64\*
-7z a -tzip ..\..\..\publish\sedio-server-osx-x64 ..\..\..\publish\server\osx-x64\*
-7z a -tzip ..\..\..\publish\sedio-server-win-x64 ..\..\..\publish\server\win-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-server-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\linux-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-server-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\osx-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-server-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\win-x64\*
 
 cd ..\..\..\
 
@@ -18,9 +28,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\agent\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\agent\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\agent\win-x64
 
-7z a -tzip ..\..\..\publish\sedio-agent-linux-x64 ..\..\..\publish\agent\linux-x64\*
-7z a -tzip ..\..\..\publish\sedio-agent-osx-x64 ..\..\..\publish\agent\osx-x64\*
-7z a -tzip ..\..\..\publish\sedio-agent-win-x64 ..\..\..\publish\agent\win-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-agent-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\linux-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-agent-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\osx-x64\*
+7z a -tzip -saa ..\..\..\publish\sedio-agent-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\win-x64\*
 
 cd ..\..\..\
 
