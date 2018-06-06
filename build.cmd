39c22bb1cd92d3e63e40e7e10f64498819e8bc01
Bernhard Glück <business@bernhardglueck.com>
2018-06-07 00:52:52 +0200

    Fixing compression output

--- build.cmd
+++ build.cmd
@@ -5,9 +5,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\server\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\server\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\server\win-x64
 
-7z a ..\..\..\publish\sedio-server-linux-x64-zip ..\..\..\publish\server\linux-x64\*
-7z a ..\..\..\publish\sedio-server-osx-x64-zip ..\..\..\publish\server\osx-x64\*
-7z a ..\..\..\publish\sedio-server-win-x64-zip ..\..\..\publish\server\win-x64\*
+7z a -tzip ..\..\..\publish\sedio-server-linux-x64 ..\..\..\publish\server\linux-x64\*
+7z a -tzip ..\..\..\publish\sedio-server-osx-x64 ..\..\..\publish\server\osx-x64\*
+7z a -tzip ..\..\..\publish\sedio-server-win-x64 ..\..\..\publish\server\win-x64\*
 
 cd ..\..\..\
 
@@ -18,9 +18,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\agent\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\agent\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\agent\win-x64
 
-7z a ..\..\..\publish\sedio-agent-linux-x64-zip ..\..\..\publish\agent\linux-x64\*
-7z a ..\..\..\publish\sedio-agent-osx-x64-zip ..\..\..\publish\agent\osx-x64\*
-7z a ..\..\..\publish\sedio-agent-win-x64-zip ..\..\..\publish\agent\win-x64\*
+7z a -tzip ..\..\..\publish\sedio-agent-linux-x64 ..\..\..\publish\agent\linux-x64\*
+7z a -tzip ..\..\..\publish\sedio-agent-osx-x64 ..\..\..\publish\agent\osx-x64\*
+7z a -tzip ..\..\..\publish\sedio-agent-win-x64 ..\..\..\publish\agent\win-x64\*
 
 cd ..\..\..\
 
