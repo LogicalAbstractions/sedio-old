3bc3a338008a0e00995df940780f6a13f1bfe44a
Bernhard Glück <business@bernhardglueck.com>
2018-06-07 02:52:53 +0200

    Appveyor ci path hack

--- build.cmd
+++ build.cmd
@@ -15,9 +15,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\server\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\server\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\server\win-x64
 
-7z a -tzip -saa ..\..\..\publish\sedio-server-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\linux-x64\*
-7z a -tzip -saa ..\..\..\publish\sedio-server-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\osx-x64\*
-7z a -tzip -saa ..\..\..\publish\sedio-server-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\win-x64\*
+7z a -tzip -saa ..\..\..\sedio-server-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\linux-x64\*
+7z a -tzip -saa ..\..\..\sedio-server-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\osx-x64\*
+7z a -tzip -saa ..\..\..\sedio-server-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\server\win-x64\*
 
 cd ..\..\..\
 
@@ -28,9 +28,9 @@ dotnet publish -r linux-x64 -c Release -o ..\..\..\publish\agent\linux-x64
 dotnet publish -r osx-x64 -c Release -o ..\..\..\publish\agent\osx-x64
 dotnet publish -r win-x64 -c Release -o ..\..\..\publish\agent\win-x64
 
-7z a -tzip -saa ..\..\..\publish\sedio-agent-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\linux-x64\*
-7z a -tzip -saa ..\..\..\publish\sedio-agent-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\osx-x64\*
-7z a -tzip -saa ..\..\..\publish\sedio-agent-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\win-x64\*
+7z a -tzip -saa ..\..\..\sedio-agent-linux-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\linux-x64\*
+7z a -tzip -saa ..\..\..\sedio-agent-osx-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\osx-x64\*
+7z a -tzip -saa ..\..\..\sedio-agent-win-x64-%APPVEYOR_BUILD_VERSION% ..\..\..\publish\agent\win-x64\*
 
 cd ..\..\..\
 
